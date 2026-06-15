8869da979b39dac692b64df1ba35dfc4ae81e5d1
mariaisabelemn-lang <mariaisabelemn@gmail.com>
2026-06-15 12:37:46 -0300

    começando projeto 3bimestre

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function calcular(){
+    let num1 = document.getElementById
+    ('edtnum1').value;
+    let num2 = document.getElementById
+    ('edtnum2').value;
+    let sub = num1 - num2;
+    console.log('Subtração = '+sub);
+    let mult = num1 * num2;
+    console.long('multiplicação = '+mult);
+    let soma = numbernum1 + num2;
+    console.long('adição = '+adi);
+    let div = num1 / num2;
+    console.long('divisão = '+div);
+}
