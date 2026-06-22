69ef777bdc495e8f1d17e4d35fd9f37de6ad8a2d
mariaisabelemn-lang <mariaisabelemn@gmail.com>
2026-06-22 12:39:23 -0300

    projeto 3bimestre

--- js/script.js
+++ js/script.js
@@ -1,14 +1,42 @@
-function calcular(){
-    let num1 = document.getElementById
-    ('edtnum1').value;
-    let num2 = document.getElementById
-    ('edtnum2').value;
-    let sub = num1 - num2;
+function calcular() {
+    let resultado= document.getElementById('resultado');
+    let num1 = document.getElementById ('edtnum1').value;
+    let num2 = document.getElementById ('edtnum2').value;
+    resultado.innerHTML = '';
+    let soma = Number(num1) + Number(num2);
+    console.log('soma = '+ soma);
+    resultado.innerHTML+= '<h3>soma = '+soma+'</h3>';
+    let sub = Number(num1) - Number(num2);
     console.log('Subtração = '+sub);
-    let mult = num1 * num2;
-    console.long('multiplicação = '+mult);
-    let soma = numbernum1 + num2;
-    console.long('adição = '+adi);
-    let div = num1 / num2;
-    console.long('divisão = '+div);
+    resultado.innerHTML+= '<h3>subtração = '+sub+'</h3>';
+     let div = Number(num1) / Number(num2);
+    console.log('divisão = '+div);
+    resultado.innerHTML+= '<h3>divisao = '+div+'</h3>';
+    let mult = Number(num1) * Number(num2);
+    console.log('multiplicação = '+mult);
+    resultado.innerHTML+= '<h3>multiplicação = '+mult+'</h3>';
+}
+
+function exibirTabuada(){
+    let mostratabuada = document.getElementById('mostratabuada');
+    let numero = document.getElementById('edtnumero').value;
+    let i = 0;
+    mostratabuada.innerHTML='';
+    while(i<=10){
+        mostratabuada.innerHTML += '<h4>'+numero+' x '+i+' = '+(numero*i) +'</h4>';
+        i++;
+    }
+}
+
+function vereficaMaior(){
+    let n1 = document.getElementById('edtn1').value;
+    let n2 = document.getElementById('edtn2').value;
+    let maiornumero = document.getElementById('maiornumero');
+    maiornumero.innerHTML = '';
+    if(n1>n2){
+        maiornumero.innerHTML = '<h2>0 maior número digitado é '+n1+'</h2>';
+    }else{
+        maiornumero.innerHTML = '<h2>0 maior número digitado é '+n2+'</h2>';
+        
+    }
 }
